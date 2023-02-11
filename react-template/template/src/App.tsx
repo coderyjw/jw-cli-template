e7e08bd5bcf8409d293880abdb17a152782f9ed0
叶继伟 <yejw6@asiainfo.com>
2023-02-12 03:59:29 +0800

    fix: react 模板修改

--- react-template/template/src/App.tsx
+++ react-template/template/src/App.tsx
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from 'react'
+import { useRoutes, Link } from 'react-router-dom'
+import routes from './router'
 
+import { useAppSelector, shallowEqualApp, useAppDispatch } from './store'
+import { incremented, decremented } from '@/store/modules/counter'
 function App() {
+  const { count } = useAppSelector(
+    (state) => ({
+      count: state.counter.count
+    }),
+    shallowEqualApp
+  )
+
+  const dispatch = useAppDispatch()
+  function addCount() {
+    dispatch(incremented())
+  }
+  function subCount() {
+    dispatch(decremented())
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="nav">
+        <Link to="/home">菜单一</Link>
+        <Link to="/mine">菜单二</Link>
+      </div>
+      <Suspense fallback="loading...">
+        <div className="main">{useRoutes(routes)}</div>
+
+        <div>count：{count}</div>
+        <button onClick={subCount}>-1</button>
+        <button onClick={addCount}>+1</button>
+      </Suspense>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ react-template/template/src/service/request/types.ts
@@ -0,0 +1,14 @@
+import type { AxiosRequestConfig, AxiosResponse } from 'axios'
+
+// 针对AxiosRequestConfig配置进行扩展
+export interface Interceptors<T = AxiosResponse> {
+  requestSuccessFn?: (config: AxiosRequestConfig) => AxiosRequestConfig
+  requestFailureFn?: (err: any) => any
+  responseSuccessFn?: (res: T) => T
+  responseFailureFn?: (err: any) => any
+}
+
+export interface RequestConfig<T = AxiosResponse>
+  extends Partial<AxiosRequestConfig> {
+  interceptors?: Interceptors<T>
+}
